a1f67687025cae4c3ca54560138a9b495779a9f9
Susan Lippa <sfl@dhcp-10-102-209-252.wireless.northwestern.private>
2018-01-26 09:38:58 -0600

    goal number in

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,13 +2,21 @@
 
 $(document).ready(function() {
 
-var wins=0;
+var wins = 0;
 var losses = 0;
-goalNum;
-playerNum;
+
 
 
 var startGame = function(){
+
+//id goalNum
+var goalNumber = function() {
+    min = Math.ceil(19);
+    max = Math.floor(120);
+    return Math.floor(Math.random() * (120 - 19 + 1)) +1;
+}
+console.log(goalNumber());
+document.querySelector("#goalNum").innerText = goalNumber();
 
 
 
