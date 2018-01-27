097d1903999521cbcfd306572b33d7825c3a09cb
Susan Lippa <sfl@dhcp-10-102-209-252.wireless.northwestern.private>
2018-01-27 14:30:41 -0600

    most functionality works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,74 +3,95 @@
 $(document).ready(function() {
 
 var wins = 0;
-var losses = 0;
+var loss = 0;
+var gemSum;
+var goalNumber;
 
 //Start of Game
 
-var startGame = function() {
+        var startGame = function() {
 
-var goalNumber = 0;
-var gemSum = 0;
-$("#goalNum").text(0); 
-$("#scoreNum").text(0); 
-$("#inxsText").text("Press the colored gems to generate your score.");
+                goalNumber = 0;
+                gemSum = 0;
+                $("#goalNum").text(0); 
+                $("#scoreNum").text(0); 
+                $("#inxsText").text("Press the colored gems to generate your score.");
+} 
 
-//id goalNum
-        var goalNumber = function() {
+startGame();
+
+
+        var createGoal = function() {
                 min = Math.ceil(19);
                 max = Math.floor(120);
                 return Math.floor(Math.random() * (120 - 19 + 1)) +19;   
         }
-        $("#goalNum").text(goalNumber);  
+        
+createGoal();
+        
+        goalNumber = createGoal();
+        console.log(goalNumber);
+        $("#goalNum").text(goalNumber); 
 
-// id crystal#
+
         var crystalNumber = function() {
                 min = Math.ceil(1);
                 max = Math.floor(12);
                 return (Math.floor(Math.random() * (12 - 1 + 1)) +1);
         }
 
-
         for (i = 0; i < 4; i++) {
                 var crystalValue = crystalNumber();
+                //console.log(crystalValue);
                 $("#crystal" + (i + 1)).text(crystalValue);
-                $("#crystal" + (i + 1)).attr("crystal-val", crystalValue)
+                $("#crystal" + (i + 1)).attr("crystal-val", crystalValue);
         }
 
 // crystal calculations
 
         var chooseCrystal = function() {
-           // alert("crystal clicked");
-
+           
                 var gemPress = parseInt($(this).attr("crystal-val"));
                                 console.log($(this).attr("crystal-val"));
-                        gemSum = gemSum + gemPress;
+                                console.log(gemPress);
+                        
+                         gemSum = gemSum + gemPress;
+                                console.log(gemSum);
                         $("#scoreNum").text(gemSum);
 
-                        if (gemSum === goalNumber) {
+
+                var compareScores = function() {
+                        console.log(goalNumber);
+                        console.log(gemSum);
+                        if (goalNumber === gemSum) {
+                                alert("Wins");
                                 wins++;
                                 $("#winsNum").text(wins);
                                 $("#inxsText").text("You win! Press the blue gem if you want to play again.");
 
-                        } else if (gemSum > goalNumber) {
-                                losses++
-                                $("#lossesNum").text(losses);
+                        } else if (goalNumber < gemSum) {
+                                alert("Loss");
+                                loss++;
+                                console.log(loss);
+                                $("#lossNum").text(loss);
                                 $("#inxsText").text("You lose! Press the blue gem if you want to play again");
 
                         } else {}
 
-        } //chooseCrystal
+                }
+                compareScores();
 
-
-} // startGame close
-
+}
+$(".crystal").on("click", chooseCrystal);
 
 
 $("#blueCrystal").on("click", startGame);
-$(".crystal").on("click", chooseCrystal);
+
+
+
 
 //document.removeEventListener("click", startGame);
-// document.addEventListener("click", startGame);
+//document.addEventListener("click", startGame);
 
 
 }); //readyFunction close
